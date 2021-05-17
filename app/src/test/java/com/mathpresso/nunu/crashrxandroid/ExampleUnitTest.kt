50c73250a4410e41e653139366bad8f31b81cbe8
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-05-17 15:06:09 +0900

    [TEST] 구구단 코드 추가

--- app/src/test/java/com/mathpresso/nunu/crashrxandroid/ExampleUnitTest.kt
+++ app/src/test/java/com/mathpresso/nunu/crashrxandroid/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.mathpresso.nunu.crashrxandroid
 
+import io.reactivex.Observable
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,20 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun `구구단 만들기 with Observable`() {
+        val myNumber = 7
+        Observable.fromIterable((1..9))
+            .map { it -> println("$it * $myNumber = ${it * myNumber}") }
+            .subscribe()
+
+        Observable.just(myNumber + 1)
+            .flatMap { it1 ->
+                Observable.range(1, 9).map { it2 ->
+                    "$it1 * $it2 = ${it1 * it2}"
+                }
+            }.subscribe { println(it) }
     }
 }
